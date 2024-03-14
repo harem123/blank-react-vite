c8adb667e262a7e6ba48b0f4c43c990bb8a53d41
Camilo Portilla <cp1085275@gmail.com>
2024-03-14 07:30:17 -0500

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>Choo Choo! This is an example of a Vite + React app running on Railway.</p>
+      <div className=" mt-80 flex flex-col items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-5xl text-indigo-950  font-bold">Calidad de Generacion</h1>
+          <h4 className="mt-4 text-gray-500">Ingrese los productos a consultar para obtener un listado de las fechas de la ultima orden y la ultima anomalia</h4>
+        </div>
+        <div className="mt-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+              Productos
+            </label>
+        </div>
+        <div className="flex flex-row items-center space-x-2">
+          <div className="flex flex-col">
+            
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Productos"/>
+          </div>
+          <button className="bg-indigo-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Consultar
+          </button>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
 export default App
